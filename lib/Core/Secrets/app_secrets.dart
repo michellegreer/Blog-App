38f14245343cc082b7af2bbb2deca71f836c0845
Michelle Greer <michellegreer@Michelles-MacBook-Air-2.local>
2026-06-03 21:04:34 -0500

    Add fallback values to secrets for Vercel deployment

--- lib/Core/Secrets/app_secrets.dart
+++ lib/Core/Secrets/app_secrets.dart
@@ -1,13 +1,22 @@
 class AppSecrets {
-  static const supaBaseUrl =
-      String.fromEnvironment('SUPABASE_URL');
+  static const supaBaseUrl = String.fromEnvironment(
+    'SUPABASE_URL',
+    defaultValue: 'https://irroysbfmvchllwgvnrn.supabase.co',
+  );
 
-  static const supaBaseAnon =
-      String.fromEnvironment('SUPABASE_ANON_KEY');
+  static const supaBaseAnon = String.fromEnvironment(
+    'SUPABASE_ANON_KEY',
+    defaultValue:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlycm95c2JmbXZjaGxsd2d2bnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MTE0MjQsImV4cCI6MjA5NTk4NzQyNH0.xKjYGJAZAiNdA9eRA4-RPFQS3e2rWZQT6BfIuppioRY',
+  );
 
-  static const resendAPIKey =
-      String.fromEnvironment('RESEND_API_KEY');
+  static const resendAPIKey = String.fromEnvironment(
+    'RESEND_API_KEY',
+    defaultValue: 're_WqAGt7SR_CbkBNztTiePTGL5g3ti1tRe9',
+  );
 
-  static const adminEmail =
-      String.fromEnvironment('ADMIN_EMAIL');
+  static const adminEmail = String.fromEnvironment(
+    'ADMIN_EMAIL',
+    defaultValue: 'michelle@michellesblog.net',
+  );
 }
